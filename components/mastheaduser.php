a8b121ce8417cf8d2391ff487e7d8f3cd1485e5f
Tuitune <71855222+Tuitune@users.noreply.github.com>
2020-09-25 00:44:36 +0100

    Add files via upload

---
+++ components/mastheaduser.php
@@ -0,0 +1,32 @@
+<?php 
+echo ' 
+<html lang="en">
+	<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <title>Tuitune</title>
+
+    <link href="style/cover.css" rel="stylesheet">
+
+  </head>
+  <body>
+
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="masthead">
+    <div class="inner">
+      <h3 class="masthead-brand"><a href="index.php" style="text-decoration: none; color: black;">Tuitune</a></h3>
+      <nav class="nav nav-masthead justify-content-center">
+       <a class="nav-link" href="#">Home</a>
+       <a class="nav-link" href="#">Profile</a>
+       <a class="nav-link" href="#">Settings</a>
+       <a class="nav-link" href="php/logoutaction.php">Log out</a>
+      </nav>
+    </div>
+
+  </header>';
+?>
